20d32bbbe88f9d5559792b6f51fb3ab667f30215
Ski's <volocajdaniil0@gmail.com>
2026-06-11 00:57:51 +0500

    admin pass and minor fixs

---
+++ auth.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).end();
+
+  const { password } = req.body;
+  if (!password) return res.status(400).json({ ok: false });
+
+  if (password === process.env.ADMIN_PASSWORD) {
+    res.status(200).json({ ok: true });
+  } else {
+    res.status(401).json({ ok: false });
+  }
+}
